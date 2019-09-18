6b0f3bfb8b3bbd687d341db6bc632b9c83b507e7
Rutsinsukmee <rutsin_sukmee@hotmail.com>
2019-09-18 19:56:27 +0700

    jayjay V+C

--- app/Http/Controllers/Report/InventoryController.php
+++ app/Http/Controllers/Report/InventoryController.php
@@ -7,5 +7,19 @@ use App\Http\Controllers\Controller;
 
 class InventoryController extends Controller
 {
-    //
+    public function screen_3_2(){
+        return view('report.inventory.screen_3_2');
+    }
+
+    public function screen_3_6(){
+        return view('report.inventory.screen_3_6');
+    }
+
+    public function screen_3_13(){
+        return view('report.inventory.screen_3_13');
+    }
+
+    public function screen_4_4(){
+        return view('report.inventory.screen_4_4');
+    }
 }
--- app/Http/Controllers/Report/SalesController.php
+++ app/Http/Controllers/Report/SalesController.php
@@ -7,5 +7,36 @@ use App\Http\Controllers\Controller;
 
 class SalesController extends Controller
 {
-    //
+    public function screen_1_3(){
+        return view('report.sales.screen_1_3');
+    }
+
+    public function screen_1_5(){
+        return view('report.sales.screen_1_5');
+    }
+
+    public function screen_1_12(){
+        return view('report.sales.screen_1_12');
+    }
+
+    public function screen_1_15(){
+        return view('report.sales.screen_1_15');
+    }
+
+    public function screen_1_16(){
+        return view('report.sales.screen_1_16');
+    }
+
+    public function screen_1_18(){
+        return view('report.sales.screen_1_18');
+    }
+
+    public function screen_1_19(){
+        return view('report.sales.screen_1_19');
+    }
+
+    public function screen_1_21(){
+        return view('report.sales.screen_1_21');
+    }
+
 }
