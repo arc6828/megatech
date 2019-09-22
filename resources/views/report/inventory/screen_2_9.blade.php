2d2ad9e09642e1ecf68b2dfcc060d9e237ea1fe4
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2019-09-22 16:20:49 +0700

    Lucky

---
+++ resources/views/report/inventory/screen_2_9.blade.php
@@ -0,0 +1,56 @@
+<body>
+<h1 style="text-align:center">บริษัท เมก้า คัตติ้งทูล จำกัด</h1>
+<p style="text-align:center">รายงานแสดงรายละเอียดต้นทุน/จำนวน(FIFO)<br>
+พิมพ์ ณ วันที่ : 03/09/2019</p>
+<p style="text-indent: 4em;">ตั้งแต่รหัสสินค้า 2ALE30200S06 ถึง ZYHOLDER04 , ตั้งแต่วันที่ 01/09/2019 - 30/09/2019 </p>
+
+
+<table align="center" width="1400" border="0" padding-top="0" cellspacing="0" style="border-top:solid 1px #000; text-align:center;">
+<tr>
+    <td style="border-bottom: solid 1px #000;" width="180"><b>รหัส</td>
+    <td style="border-bottom: solid 1px #000;" width="500"><b>รายละเอียด</td>
+    <td style="border-bottom: solid 1px #000;" width="100"><b>จำนวน<br>ยกมา</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>จำนวน<br>รับ</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>จำนวน<br>ออก</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>จำนวนคงเหลือ</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>หน่วย</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>มูลค่า<br>ยกมา</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>มูลค่า<br>รับ</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>มูลค่า<br>ออก</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>มูลค่าคงเหลือ</td>
+    <td style="border-bottom: solid 1px #000;"width="100"><b>ต้นทุนเฉลี่ย</td>
+  </tr>
+
+  <tr height="40">
+    <td align = 'left'>TT6000491X</td>
+    <td align = 'left'>TDC 3 &nbsp; TT9030</td>
+    <td align = 'right'>110.00</td>
+    <td align = 'right'>100.00</td>
+    <td></td>
+    <td align = 'right'>120.00</td>
+    <td>PCS</td>
+    <td align = 'right'>40,700.00</td>
+    <td align = 'right'>3,700.00</td>
+    <td align = 'right'></td>
+    <td align = 'right'>44,400.00</td>
+    <td align = 'right'>370.00</td>
+  </tr>
+
+  <tr height="40">
+    <td align = 'left'>TT6000490X</td>
+    <td align = 'left'>TDC 2 &nbsp; TT9030</td>
+    <td align = 'right'>205.00</td>
+    <td align = 'right'>50.00</td>
+    <td></td>
+    <td align = 'right'>255.00</td>
+    <td>PCS</td>
+    <td align = 'right'>66,625.00</td>
+    <td align = 'right'>16,250.00</td>
+    <td align = 'right'></td>
+    <td align = 'right'>82,875.00</td>
+    <td align = 'right'>325.00</td>
+  </tr>
+
+</table>
+</div>
+</body>
