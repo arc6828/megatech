107cf2c226bd5d935b0202f12cb2decaf41c639f
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-03-12 12:54:13 +0700

    1_21 not completed

--- resources/views/report/sales/screen_1_21.blade.php
+++ resources/views/report/sales/screen_1_21.blade.php
@@ -2,10 +2,10 @@
 <h3>วิเคราะห์ขาย จำนวนขาย ลูกค้า สินค้าทั้งปี</h3>
 <h3>พิมพ์ ณ วันที่ : 08/08/2019</h3>
 <h2>ตั้งแต่วันที่ 01/08/2019 - 31/08/2019 , ตั้งแต่รหัสลูกค้า A0001 ถึง Z0002 , ตั้งแต่รหัสสินค้า 2ALE030200S06 ถึง ZYHOLDER04</h2>
-<table border=1>
+<table border=1 style="width:100%">
 	<tr>
 		<th>รหัสสินค้า</th>
-		<th colspan="2">รายละเอียดสินค้า</th>
+		<th>รายละเอียดสินค้า</th>
 		<th>เดือน 1</th>
 		<th>เดือน 2</th>
 		<th>เดือน 3</th>
@@ -25,27 +25,27 @@
 		@foreach ($customers->Invoice as $invoices)
 			<tr>
 				<td><strong>รหัสลูกค้า</strong></td> 
-				<td><strong>{{ $customers->customer_code }}</strong></td>
-				<td col	span="2"><strong>{{ $customers->company_name }}</strong></td>
+				<td colspan="14"><strong>{{ $customers->customer_code }} {{ $customers->company_name }}</strong></td>
 			</tr>
 			@foreach ($invoices->InvoiceDetail as $InvoiceDetail)
 				<tr>
 					<td>{{ $InvoiceDetail->Product->product_code }}</td>
-					<td>{{ $InvoiceDetail->Product->product_detail }}</td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td></td>
+					<td>{{ $InvoiceDetail->Product->product_name }} {{ $InvoiceDetail->Product->grade }}</td>
+					
+					
+					<td class="number">  {{number_format($customers->invoices_by_month(1)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(2)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(3)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(4)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(5)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(6)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(7)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(8)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(9)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(10)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(11)->sum('total'),2) }}</td>
+					<td class="number">  {{number_format($customers->invoices_by_month(12)->sum('total'),2)}}</td>
+					<td class="number"> {{number_format($customers->invoices->sum('total'),2)}}</td>
 				</tr>		
 			@endforeach
 		@endforeach
