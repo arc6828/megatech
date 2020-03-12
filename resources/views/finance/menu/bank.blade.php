b0d6c3a6bdc3d202d42398e31be1c63c70941799
arc6828 <chavalit.kow@gmail.com>
2020-03-12 17:01:56 +0700

    Update bank.blade.php

--- resources/views/finance/menu/bank.blade.php
+++ resources/views/finance/menu/bank.blade.php
@@ -56,7 +56,26 @@
             <div><br>ค่าใช้จ่าย ธ.</div>        
           </div>
         </a> 
-      </div>       
+      </div>     
+      <div class="col-6 col-md-3">
+        
+      </div>   
+      <div class="col-6 col-md-3">
+        <a class="btn btn-outline-primary btn-menu  my-3" href="{{url('/')}}/finance/bank-transaction?transaction_code=cash-transfer-in">
+          <div class="vertical-center">
+            <div class="px-4"><i class="round round-primary my-2 ni ni-single-copy-04"></i></div>
+            <div><br>ชำระโดยลูกค้า</div>        
+          </div>
+        </a> 
+      </div> 
+      <div class="col-6 col-md-3">
+        <a class="btn btn-outline-primary btn-menu  my-3" href="{{url('/')}}/finance/bank-transaction?transaction_code=cash-transfer-out">
+          <div class="vertical-center">
+            <div class="px-4"><i class="round round-primary my-2 ni ni-single-copy-04"></i></div>
+            <div><br>ชำระเจ้าหนี้</div>        
+          </div>
+        </a> 
+      </div> 
     </div>      
   </div>    
 </div> 
