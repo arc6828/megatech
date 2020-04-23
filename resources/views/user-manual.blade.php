4c9073ed79001ac2b3becb0790e70edaf8bded93
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-04-23 17:32:59 +0700

    Update user-manual.blade.php

--- resources/views/user-manual.blade.php
+++ resources/views/user-manual.blade.php
@@ -12,11 +12,13 @@
             <div class="card-body">
                 <ul>
                     <li><a href="https://docs.google.com/presentation/d/11vjr4Nv7_gXYRdJ4W2h3p2x-6q5xyk_WgSUymw-oo6k/edit?usp=sharing">การขาย</a></li>
-                    <li><a href="#">การซื้อ</a></li>
+                    <li><a href="https://docs.google.com/presentation/d/1u2OFj_cmEZuxbV5NIsIovzYkzM-KVKBsLWaLNQI1TwA/edit?usp=sharing">การซื้อ</a></li>
                     <li><a href="#">คงคลัง</a></li>
                     <li><a href="#">การเงิน</a></li>
                     <li><a href="#">รายงาน</a></li>
                     <li><a href="#">อื่นๆ</a></li>
+                    <li><a href="#">แฟ้มลูกหนี้</a></li>
+                    <li><a href="#">แฟ้มเจ้าหนี้</a></li>
                 </ul>
             </div>
                     
