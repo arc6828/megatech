1d26059264157154de334120984c9a06387b146b
arc6828 <chavalit.kow@gmail.com>
2020-05-28 11:04:33 +0700

    Update user-manual.blade.php

--- resources/views/user-manual.blade.php
+++ resources/views/user-manual.blade.php
@@ -13,12 +13,10 @@
                 <ul>
                     <li><a href="https://docs.google.com/presentation/d/11vjr4Nv7_gXYRdJ4W2h3p2x-6q5xyk_WgSUymw-oo6k/edit?usp=sharing">การขาย</a></li>
                     <li><a href="https://docs.google.com/presentation/d/1u2OFj_cmEZuxbV5NIsIovzYkzM-KVKBsLWaLNQI1TwA/edit?usp=sharing">การซื้อ</a></li>
-                    <li><a href="#">คงคลัง</a></li>
-                    <li><a href="#">การเงิน</a></li>
-                    <li><a href="#">รายงาน</a></li>
+                    <li><a href="https://docs.google.com/presentation/d/11GAL06I1X0WkhF_6I0ZXKjutimHZq-Rgx8Hr-5-HaPE/edit?usp=sharing">คงคลัง</a></li>
+                    <li><a href="https://docs.google.com/presentation/d/1tnYkHDrb_kMRiR86yHdbt3CLoSLAacv0izAAMh4Yums/edit?usp=sharing">การเงิน</a></li>
+                    <li><a href="#">รายงาน (ยังสร้างรายงานไม่เสร็จ)</a></li>
                     <li><a href="#">อื่นๆ</a></li>
-                    <li><a href="#">แฟ้มลูกหนี้</a></li>
-                    <li><a href="#">แฟ้มเจ้าหนี้</a></li>
                 </ul>
             </div>
                     
