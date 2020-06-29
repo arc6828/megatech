ecbca6c6bb8656861dde467d0044306e4b5c0e90
arc6828 <chavalit.kow@gmail.com>
2020-06-29 14:19:02 +0700

    migrate add_total_debt

---
+++ database/migrations/2020_06_29_141402_add_total_debt_supplier_debts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTotalDebtSupplierDebtsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('supplier_debts', function (Blueprint $table) {
+            $table->float('total_debt',16,2)->nullable();
+            $table->float('discount',16,2)->nullable()->change();
+            $table->float('vat_percent',16,2)->nullable()->change();
+            $table->float('vat',16,2)->nullable()->change();
+            $table->float('total_before_vat',16,2)->nullable()->change();
+            $table->float('total',16,2)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('supplier_debts', function (Blueprint $table) {
+            //
+        });
+    }
+}
