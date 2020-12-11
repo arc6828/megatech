7c60a1023adc9d854b71d4464cb2d0d7c5624d34
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-12-11 14:37:16 +0700

    Cheaqe

--- resources/views/cheque/index.blade.php
+++ resources/views/cheque/index.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts/argon-dashboard/theme')
+@php
+    $main = request('cheque_type_code');
+    switch($main){
+        case "cheque-in" : 
+            $main = "ทะเบียนเช็ครับ";
+            break;              
+        case "cheque-out" : 
+            $main = "ทะเบียนเช็คจ่าย";
+            break;        
+        case "" : 
+            $main = "ทั้งหมด";
+            break;        
+    }
+@endphp
 
-@section('title','ทะเบียนเช็ค : '.request('cheque_type_code'))
+@section('level-0-url', url('finance')."?tab=cheque-tab")
+@section('level-0','การเงินเช็ค')
+
+@section('title', $main )
+
+@section('background-tag','bg-info ')
 
 @section('content')
     <div class="container">
@@ -8,9 +27,9 @@
 
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">@yield('title')</div>
+                    <!-- <div class="card-header">@yield('title')</div> -->
                     <div class="card-body">
-                        <a href="{{ url('/finance') }}?tab=cheque-tab" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>                        
+                        <!-- <a href="{{ url('/finance') }}?tab=cheque-tab" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>                        
                         <a href="{{ url('/finance/cheque/create') }}?cheque_type_code={{request('cheque_type_code')}}" class="btn btn-success btn-sm" title="Add New Cheque">
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
                         </a>
@@ -27,25 +46,25 @@
                         </form>
 
                         <br/>
-                        <br/>
+                        <br/> -->
                         <div class="table-responsive">
-                            <table class="table">
+                            <table class="table table-sm table-hover text-center table-bordered table-striped" id="table">
                                 <thead>
                                     <tr>
-                                        <th class="d-none">Cheque Type</th>
-                                        <th>Date</th>
-                                        <th>Cheque No</th>
-                                        <th>Total</th>
+                                        <!-- <th class="d-none">Cheque Type</th> -->
+                                        <th>วันที่ออกเช็ค</th>
+                                        <th>เลขที่เช็ค</th>
+                                        <th>ยอดเงิน</th>
                                         
-                                        <th>Bank Account Id</th>
-                                        <th>Status</th>
+                                        <th>บัญชีธนาคาร</th>
+                                        <th>สถานะ</th>
                                         <th>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                 @foreach($cheque as $item)
                                     <tr>
-                                        <td class="d-none">{{ $item->cheque_type_code }}</td>                                 
+                                        <!-- <td class="d-none">{{ $item->cheque_type_code }}</td>                                  -->
                                         <td>{{ $item->cheque_date }}</td>
                                         <td>{{ $item->cheque_no }}</td>       
                                         <td>{{ $item->total }}</td>   
@@ -74,7 +93,7 @@
                                             @endif
                                         </td>
                                         
-                                        <td class="d-none">
+                                        <!-- <td class="d-none">
                                             <a href="{{ url('/finance/cheque/' . $item->id) }}" title="View Cheque"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
                                             <a href="{{ url('/finance/cheque/' . $item->id . '/edit') }}" title="Edit Cheque"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
 
@@ -83,12 +102,16 @@
                                                 {{ csrf_field() }}
                                                 <button type="submit" class="btn btn-danger btn-sm" title="Delete Cheque" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
                                             </form>
-                                        </td>
+                                        </td> -->
                                     </tr>
                                 @endforeach
                                 </tbody>
                             </table>
-                            <div class="pagination-wrapper"> {!! $cheque->appends(['search' => Request::get('search')])->render() !!} </div>
+                        </div>
+                        <div class="text-center mt-4">
+                            <a href="{{ url('/finance/cheque/create') }}?cheque_type_code={{request('cheque_type_code')}}" class="btn btn-success" title="Add New Cheque">
+                                <i class="fa fa-plus" aria-hidden="true"></i> {{ $main }}
+                            </a>
                         </div>
 
                     </div>
@@ -96,4 +119,30 @@
             </div>
         </div>
     </div>
+    <script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        let table = $('#table').DataTable({          
+            // ordering: false,                  
+            paging: false,
+            info: false,        
+            order : [0, 'desc']  
+            // searching: false,                
+        }); //END DataTable
+
+        //DATA TABLE SCROLL
+        let tableCont = document.querySelector('#table');        		
+        tableCont.style.cssText  = "margin-top : -1px !important; width:100%;";
+        tableCont.parentNode.style.overflow = 'auto';
+        tableCont.parentNode.style.maxHeight = '400px';
+        tableCont.parentNode.addEventListener('scroll',function (e){
+            var scrollTop = this.scrollTop;
+            this.querySelector('thead').style.transform = 'translateY(' + scrollTop + 'px) '+'translateZ(' + 100 + 'px)';
+            this.querySelector('thead').style.background = "white";
+            this.querySelector('thead').style.zIndex = "3000";
+            this.querySelector('thead').style.marginBottom = "100px";
+            //console.log(scrollTop);
+        })
+        //END DATA TABLE SCROLL
+    });
+    </script>
 @endsection
--- resources/views/layouts/argon-dashboard/footer.blade.php
+++ resources/views/layouts/argon-dashboard/footer.blade.php
@@ -2,14 +2,14 @@
   <div class="row align-items-center justify-content-xl-between">
     <div class="col-xl-6">
       <div class="copyright text-center text-xl-left text-muted">
-        &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+        &copy; {{ date('Y') }} <a href="#" class="font-weight-bold ml-1" target="_blank">Megatechcuttingtool.com</a>
       </div>
     </div>
     <div class="col-xl-6">
       <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-        </li>
+        </li> -->
         <li class="nav-item">
           <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
         </li>
